a719c1495bdd614ec05a0d04e4d70467db2750c9
Pavel Petrov <paha290585@gmail.com>
2024-04-29 02:36:40 +0800

    app without jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 function App() {
 return (
-	<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <br/>
-          <div>{(new Date().getFullYear())}</div>
-        </a>
-      </header>
-    </div>
+	createElement('div', { className: 'App' },
+		createElement('header', { className: 'App-header' },
+			createElement('img', { src: `${logo}`, className:'App-logo', alt:'logo' }, null),
+			createElement('p', null, 'Edit ',
+				createElement('code', {src: 'App.js'}, null), ' and save to reload' ),
+			createElement('a', { className:'App-link', href:'href="https://reactjs.org',
+								   target:'_blank', rel:'noopener noreferrer' },'Learn React',
+								createElement('br',null,null),
+								createElement('div',null, new Date().getFullYear()))
+							)
+						)
   );
 }
 
